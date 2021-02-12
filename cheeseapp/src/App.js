161295a66aaae46bf8f7a92c01d71590d1d1900f
maurcord <67590886+maurcord@users.noreply.github.com>
2021-02-12 13:49:09 -0500

    added universal css, added component and assests folder, as well as a pages folder. 	new file:   cheeseapp/node_modules/caniuse-lite/data/features/css-font-rendering-controls.js

--- cheeseapp/src/App.js
+++ cheeseapp/src/App.js
@@ -1,22 +1,13 @@
 
-import './App.css';
+import './Styles/App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1> HELLO WORLD!</h1>
+      
       </header>
     </div>
   );
--- readme.md
+++ readme.md
@@ -1,7 +1,7 @@
 ## Welcome to Cheese App (name pending)
 
-Pair your favorite cheese with wine! Cheese app will give you a couple suggestions on what to pair your cheese with. 
+Pair your favorite cheese with wine! Cheese app will give you a couple suggestions on what to pair your cheese with.
 
 By:
 Mauricio Cordeiro
-Ian de Medeiros 
+Ian de Medeiros
